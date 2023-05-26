e7932becc80df47e825429bc85af0beed5e2c2c2
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2023-05-26 15:18:49 +0200

    base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "/",
 });
