414613e95f0396b1e9c18588a650dd02e4d55e92
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2023-06-01 16:18:47 +0200

    Add component CheckRow.vue

---
+++ src/components/CheckRow.vue
@@ -0,0 +1,35 @@
+<script setup>
+import PeonOccurence from "@/components/PeonOccurence.vue";
+import { useColorStore } from "@/stores/ColorStore.js";
+import { storeToRefs } from "pinia";
+const color_store = useColorStore();
+const { numberOfPeons, rows } = storeToRefs(color_store);
+const checkColorsPlaces = color_store.checkColorsPlaces();
+</script>
+<template>
+  <div>
+    <PeonOccurence
+      v-for="peon in numberOfPeons"
+      :key="peon - 1"
+      :class="checkColorsPlaces[peon - 1]"
+      class="peon-in-check-row"
+    />
+  </div>
+</template>
+
+<style scoped>
+.peon-in-check-row {
+  width: 1.5vw;
+  height: 1.5vw;
+  border-radius: 50%;
+}
+.black {
+  background-color: black;
+}
+.grey {
+  background-color: grey;
+}
+.none {
+  background-color: red;
+}
+</style>
