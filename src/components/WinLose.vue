376e433fb16fea2259b710597654e79182a08bb5
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2023-06-04 15:04:04 +0200

    Create component and view for score display and message

---
+++ src/components/WinLose.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { useColorStore } from "@/stores/ColorStore.js";
+import { storeToRefs } from "pinia";
+const color_store = useColorStore();
+const winOrLoseMessage = color_store.endOfGame();
+</script>
+
+<template>
+  <div>endOfGame: {{ winOrLoseMessage }}</div>
+</template>
---
+++ src/views/ScoreView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import WinLose from "@/components/WinLose.vue";
+import { useColorStore } from "@/stores/ColorStore.js";
+import { storeToRefs } from "pinia";
+const color_store = useColorStore();
+const { score } = storeToRefs(color_store);
+</script>
+
+<template>
+  <div class="score-container">
+    SCORE: {{ score }}
+    <div>
+      <WinLose />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.score-container {
+  display: flex;
+  flex-direction: column;
+}
+</style>
