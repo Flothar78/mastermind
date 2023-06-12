ab80d034ec99cd20251c8bd269e5f47685e48678
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2023-06-12 15:55:41 +0200

    deployment and local app OK

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mastermind/",
   watchOptions: {
     poll: true,
   },
+  base: "/mastermind/",
   plugins: [vue()],
   resolve: {
     alias: {
