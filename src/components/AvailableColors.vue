db4982b419511427f6a4aa48cd8544f3b2b7e1cb
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2023-11-19 19:36:52 +0100

    chosenColorClass en cours

--- src/components/AvailableColors.vue
+++ src/components/AvailableColors.vue
@@ -1,24 +1,44 @@
 <script setup>
 import PeonOccurence from "@/components/PeonOccurence.vue";
-import { reactive, computed } from "vue";
+import { reactive, computed, ref } from "vue";
 import { useColorStore } from "@/stores/ColorStore.js";
 import { storeToRefs } from "pinia";
 const color_store = useColorStore();
-const { colorsArray, addColorToStore } = storeToRefs(color_store);
+const { colorsArray } = storeToRefs(color_store);
+const event1 = ref(null);
+const event2 = ref(null);
+const chosenColorClass = ref(false);
+event1.value = (index) => {
+  color_store.addColorToStore(color_store.colorsArray[index]);
+};
+event2.value = (index) => {
+  chosenColorClass.value = true;
+};
+const choiceColorFromAvailableColors = (index) => {
+  event1.value(index);
+  event2.value(index);
+};
 </script>
-
 <template>
   <div class="peons-row">
     <PeonOccurence
       v-for="(color, index) in colorsArray"
       :key="index"
-      :class="colorsArray[index]"
       class="available-colors-peons"
-      @click="color_store.addColorToStore(colorsArray[index])"
+      :class="
+        reactive({
+          [color]: true,
+          'chosen-color': chosenColorClass,
+        })
+      "
+      @click="choiceColorFromAvailableColors(index)"
     />
   </div>
 </template>
 
 <style scoped>
 @import "@/assets/main.css";
+.chosen-color {
+  border: 10px white solid;
+}
 </style>
