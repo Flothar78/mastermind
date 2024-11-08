875f8ca50f1e3ea5dc9da93ca03f5bcc7bdabecd
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2024-11-08 15:09:44 +0100

    reset button

--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -5,19 +5,20 @@ import TryAndChecks from "@/components/TryAndChecks.vue";
 import { useColorStore } from "@/stores/ColorStore.js";
 import { useScoreStore } from "@/stores/ScoreStore.js";
 const restartGame = () => {
-//  useColorStore().restartScoreStore();
+  //  useColorStore().restartScoreStore();
   useScoreStore().restartGame();
 };
 </script>
 <template>
+  <button type="button" class="reset-button" @click="restartGame">
+    <div>RESET</div>
+  </button>
   <div class="play-ground">
     <GameSolution />
     <TryAndChecks />
     <AvailableColors />
-    <button type="button" class="reset-button" @click="restartGame"><div>RESET</div></button>
   </div>
 </template>
 <style scoped>
 @import "@/assets/main.css";
-
 </style>
