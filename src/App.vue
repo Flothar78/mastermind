a99feaf23dfff8e0261376135d44b126a7f544ac
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2025-04-17 11:24:16 +0200

    Add reactive button to du=iplay full screen rules

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <script setup>
 import PlayView from "@/views/PlayView.vue";
 import ScoreView from "@/views/ScoreView.vue";
+import Tutorial from "./components/Tutorial.vue";
+import { ref } from "vue";
+const tutorialButton = ref(null);
+const buttonText = ref("Voir Règles");
+const gameRules = ref(false);
+const displayTutorial = () => {
+  if (gameRules.value === false) {
+    gameRules.value = true;
+    buttonText.value = "Voir Jeu";
+  } else {
+    gameRules.value = false;
+    buttonText.value = "Voir Règles";
+  }
+};
 </script>
 
 <template>
-  
-    <div class="play-and-result-wrapper">
-      <PlayView />
-    </div>
-    <ScoreView />
-  
+  <Tutorial v-if="gameRules" />
+  <div class="tutorial-button" @click="displayTutorial()" ref="tutorialButton">
+    {{ buttonText }}
+  </div>
+  <div class="play-and-result-wrapper">
+    <PlayView />
+  </div>
+  <ScoreView />
 </template>
 
 <style scoped>
