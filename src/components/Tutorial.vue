d53966e4e3889647ca33b6d080649757c10c41b7
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2025-04-23 16:29:51 +0200

    Adjust CSS small screen

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -15,7 +15,8 @@ const slides = [
     alt: "Réserve de couleurs disponibles pour le jeu",
   },
   {
-    text: "Il faut deviner la bonne combinaison en faisant des propositions, dans les rangées de gauche.",
+    text:
+      "Il faut deviner la bonne combinaison en faisant des propositions, dans les rangées de gauche.",
     img: new URL("/combinaison_couleurs.png", import.meta.url).href,
     alt: "Rangée pour proposer des combinaisons de couleurs",
   },
@@ -85,22 +86,22 @@ const nextSlide = () => {
   max-width: 1487px;
   margin: auto;
   padding: 5vh 5vw;
-  background: white;
-  opacity: 0.92;
-  z-index: 10;
+  background-color: #bdd5ea;
   font-family: sans-serif;
   font-size: 1.4rem;
+  opacity: 0.92;
   text-align: center;
+  z-index: 10;
   overflow: hidden;
 }
 .tuto-title {
-  /* border: black 1px solid; */
+  border: black 1px solid;
   position: absolute;
-  top: 0;
+  top: 4vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  height: 10vh;
+  height: fit-content;
   color: #424b54;
   font-size: 1.5rem;
   font-weight: bold;
@@ -115,6 +116,7 @@ const nextSlide = () => {
   border: 4px #424b54 solid;
   border-radius: 10%;
   box-shadow: 2px 2px 8px, -2px -2px 8px;
+  background-color: white;
 }
 .tuto-text > div {
   /* border: 2px solid black; */
@@ -141,5 +143,83 @@ img {
   font-size: 1rem;
   border-radius: 8px;
   cursor: pointer;
+}
+@media screen and (max-width: 759px) {
+  .tuto-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    height: 90vh;
+    width: 90vw;
+    max-width: 1487px;
+    margin: auto;
+    padding: 5vh 5vw;
+    background-color: #bdd5ea;
+    font-family: sans-serif;
+    font-size: 1.2rem;
+    opacity: 0.93;
+    text-align: center;
+    z-index: 10;
+    overflow: hidden;
+  }
+  .tuto-title {
+    border: black 1px solid;
+    position: absolute;
+    top: 12vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 8vh;
+    color: #424b54;
+    font-size: 1.1rem;
+    font-weight: bold;
+  }
+  .tuto-text {
+    position: absolute;
+    top: 58vh;
+    height: 44vh;
+    width: 75%;
+    max-width: 1117px;
+    padding: 4vh 4vw;
+    border: 4px #424b54 solid;
+    border-radius: 5%;
+    box-shadow: 2px 2px 8px, -2px -2px 8px;
+    background-color: white;
+  }
+  .tuto-text > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 50%;
+    text-align: center;
+  }
+  .tuto-text > div:nth-child(1) {
+    justify-content: start;
+  }
+  .tuto-text > div:nth-child(2) {
+    justify-content: end;
+  }
+  img {
+    width: 100%;
+    max-height: 100px;
+    max-width: 800px;
+    object-fit: contain;
+  }
+  .next-button {
+    height: 4vh;
+    padding: 0.8rem;
+    border: none;
+    background-color: #424b54;
+    color: white;
+    font-size: 0.8rem;
+    border-radius: 8px;
+    cursor: pointer;
+  }
 }
 </style>
